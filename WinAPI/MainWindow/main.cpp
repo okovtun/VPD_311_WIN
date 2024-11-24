ed975edf3a8cb04388987dd6e4a1134b8d07a346
User <User@DESKTOP-0TCDSR6>
2024-11-24 14:27:03 +0300

    HomeWork DONE

--- WinAPI/MainWindow/main.cpp
+++ WinAPI/MainWindow/main.cpp
@@ -1,5 +1,8 @@
 ﻿//MainWindow
+#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<cstdio>
+#include"resource.h"
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "My Main Window";
 
@@ -16,9 +19,14 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	wClass.cbClsExtra = 0;
 	wClass.cbWndExtra = 0;	//cb.... - CountBytes;
 
-	wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wClass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON_BITCOIN));
+	wClass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON_PALM));
+	//wClass.hIcon = (HICON)LoadImage(hInstance, "ICO\\bitcoin.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	//wClass.hIconSm = (HICON)LoadImage(hInstance, "ICO\\palm.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	
+	wClass.hCursor = (HCURSOR)LoadImage(
+		hInstance, "Cursors\\starcraft-original\\Working In Background.ani",IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE
+	);
 	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
 
 	wClass.hInstance = hInstance;
@@ -33,14 +41,21 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	}
 
 	//2) Создание окна:
+
+	INT screen_width = GetSystemMetrics(SM_CXSCREEN);
+	INT screen_height = GetSystemMetrics(SM_CYSCREEN);
+	INT window_width = screen_width * 3 / 4;
+	INT window_height = screen_height * .75;
+	INT window_start_x = screen_width / 8;
+	INT window_start_y = screen_height / 8;
 	HWND hwnd = CreateWindowEx
 	(
 		NULL,				//Ex-Styles
 		g_sz_WINDOW_CLASS,	//Class name
 		g_sz_WINDOW_CLASS,	//Window title
 		WS_OVERLAPPEDWINDOW,//Window style. Такой стиль всегда задается для главного окна. 
-		CW_USEDEFAULT, CW_USEDEFAULT,	//Position	- положение окна при запуске
-		CW_USEDEFAULT, CW_USEDEFAULT,	//Size		- размер создаваемого окна.
+		window_start_x, window_start_y,	//Position	- положение окна при запуске
+		window_width, window_height,	//Size		- размер создаваемого окна.
 		NULL,
 		NULL,	//hMenu: 
 				//для главного окна это ResourseID главного меню, 
@@ -76,6 +91,26 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_CREATE:
+		break;
+	case WM_MOVE:
+	case WM_SIZE:
+	{
+		RECT window_rect;
+		GetWindowRect(hwnd, &window_rect);
+		INT window_width = window_rect.right - window_rect.left;
+		INT window_height = window_rect.bottom - window_rect.top;
+
+		CONST INT SIZE = 256;
+		CHAR sz_title[SIZE]{};
+		sprintf
+		(
+			sz_title, "%s. Position:%ix%i; Size: %ix%i", 
+			g_sz_WINDOW_CLASS, 
+			window_rect.left, window_rect.top,
+			window_width, window_height
+		);
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_title);
+	}
 		break;
 	case WM_COMMAND:
 		break;
